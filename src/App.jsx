642c1925b8fc4d3bb4886befe34034aef068de8e
hilarycollado1 <hilarycollado1@gmail.com>
2024-06-12 14:50:50 +0000

    agrupando coordenadas

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,81 @@ import './App.css'
 import React from 'react'
 import 'leaflet/dist/leaflet.css'
 
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon, marker } from 'leaflet';
 
-// export default function App() {
+function App() {
 
-//   const Markers = [
-//     {
-//       getcode: [18.228074, -70.544055],
-//       popUp: "Hello world"
-//     },
-//     {
-//       getcode: [18.385408, -69.851357],
-//       popUp: "Hello world"
-//     },
-//     {
-//       getcode: [18.903831, -70.270576],
-//       popUp: "Hello world"
-//     }, {
-//       getcode: [18.909672, -70.28043],
-//       popUp: "Hello world"
-//     },
-//     {
-//       getcode: [18.910589, -70.281204],
-//       popUp: "Hello world"
-//     },
-//     {
-//       getcode: [18.911439, -70.398757],
-//       popUp: "Hello world"
-//     },
+  // COORDENADAS
+  const Markers = [
+    {
+      getcode: [18.228074, -70.544055],
+      popUp: "18.228074, -70.544055\nHello world"
+    },
+    {
+      getcode: [18.385408, -69.851357],
+      popUp: "18.385408, -69.851357\nHello world"
+    },
+    {
+      getcode: [18.903831, -70.270576],
+      popUp: "18.903831, -70.270576\nHello world"
+    }, {
+      getcode: [18.909672, -70.28043],
+      popUp: "Hello world"
+    },
+    {
+      getcode: [18.910589, -70.281204],
+      popUp: "Hello world"
+    },
+    // {
+    //   getcode: [18.911439, -70.398757],
+    //   popUp: "Hello world"
+    // },
 
-//   ]
+  ]
   // 18.228074,	-70.544055
   // 18.385408,	-69.851357
   // 18.903831,	-70.270576
   // 18.909672,	-70.28043
   // 18.910589,	-70.281204
-  // 18.911439,	-70.398757
+  // 18.911439,	-70.398757 
 
-  // }
+// ICONOS DEL MAPA 
+  const customIcon = new Icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  })
 
 
 
-  function App() {
+  // function App() {
   return (
     <div className="App">
-      <MapContainer center={[19.726728, -70.923816]} zoom={13}>
-        {/* Suggested code may be subject to a license. Learn more: ~LicenseLog:2096855377. */}
+      {/* COORDENADAS DONDE SE UBICARÁ CUANDO SE INGRESE */}
+      <MapContainer center={[18.909672, -70.28043]} zoom={10}>
+        {/* PARA LLAMAR AL MAPA */}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+
+    {/* mostrar los iconos y los mensajes */}
+        {
+          Markers.map(marker => (
+            <Marker position={marker.getcode} icon={customIcon}>
+              <Popup>
+                {marker.popUp}
+              </Popup>
+            </Marker>
+          ))
+        }
+
+      
+
       </MapContainer>
     </div>
   );
