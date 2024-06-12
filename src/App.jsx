06cfe3acba1705a60d8b058ca920f586a07b8a52
hilarycollado1 <hilarycollado1@gmail.com>
2024-06-12 15:17:54 +0000

    Agregando mapas

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 import 'leaflet/dist/leaflet.css'
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Icon, marker } from 'leaflet';
+import { Icon, divIcon, point } from 'leaflet';
+import MarkerClusterGroup from 'react-leaflet-cluster';
 
 function App() {
 
@@ -41,7 +42,7 @@ function App() {
   // 18.910589,	-70.281204
   // 18.911439,	-70.398757 
 
-// ICONOS DEL MAPA 
+  // ICONOS DEL MAPA 
   const customIcon = new Icon({
     iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
     shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
@@ -52,30 +53,45 @@ function App() {
   })
 
 
-
   // function App() {
   return (
     <div className="App">
       {/* COORDENADAS DONDE SE UBICARÁ CUANDO SE INGRESE */}
       <MapContainer center={[18.909672, -70.28043]} zoom={10}>
+
         {/* PARA LLAMAR AL MAPA */}
-        <TileLayer
+
+        {/* MAPA DE LEAFLET  */}
+        {/* <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />  */}
+
+        {/* GOOGLE MAP  */}
+        <TileLayer
+          attribution="google map"
+          url="https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
         />
 
-    {/* mostrar los iconos y los mensajes */}
-        {
-          Markers.map(marker => (
-            <Marker position={marker.getcode} icon={customIcon}>
-              <Popup>
-                {marker.popUp}
-              </Popup>
-            </Marker>
-          ))
-        }
+        {/* MarkerClusterGroup AGRUPAR LOS ICONOS Y LOS MENSAJES */}
+        <MarkerClusterGroup
+          chunkedLoading
+        // iconCreateFunction={createClusterCustomIcon}
+        >
+          {/* mostrar los iconos y los mensajes */}
+          {
+            Markers.map(marker => (
+              <Marker position={marker.getcode} icon={customIcon}>
+                <Popup>
+                  {marker.popUp}
+                </Popup>
+              </Marker>
+            ))
+          }
+        </MarkerClusterGroup>
 
-      
+
+
 
       </MapContainer>
     </div>
